66f8c10b3f2b39690fa31463833df0e33de841fe
VictoriaGalvan <victoria.m.galvan@gmail.com>
2021-01-15 10:36:49 -0500

    Finished Timer Functionality
    The application counts down from a given minute value. 5 is in place as
    a test value. It will be updated to follow the Pomodoro technique soon.
    
    Signed-off-by: VictoriaGalvan <victoria.m.galvan@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 # Created by Victoria M. Galvan
 
 # ---------------------------- IMPORTS ------------------------------- #
+import math
 from tkinter import *
-from tkinter.ttk import *
 
 # ---------------------------- SETTINGS ------------------------------- #
 # Color Settings
@@ -26,11 +26,30 @@ LONG_BREAK_MIN = 20
 WIDTH = 200
 HEIGHT = 224
 
-# ---------------------------- TIMER RESET ------------------------------- # 
+
+# ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+def start_timer():
+    countdown(5 * 60)
+
+
+def countdown(time):
+    minute = math.floor(time / 60)
+    second = time % 60
+
+    if second < 10:
+        second = f"0{second}"
+
+    new_time = f"{str(minute)}:{str(second)}"
+
+    canvas.itemconfig(timer_text, text=new_time)
+
+    if time > 0:
+        window.after(1000, countdown, time - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 # create window
@@ -43,16 +62,16 @@ tomato_img = PhotoImage(file="tomato.png")
 
 # create canvas
 canvas = Canvas(width=WIDTH, height=HEIGHT, bg=YELLOW, highlightthickness=0)
-canvas.create_image(WIDTH/2, HEIGHT/2, image=tomato_img)
-canvas.create_text(WIDTH/2, 130, text="00:00", fill="white", font=FONT)
+canvas.create_image(WIDTH / 2, HEIGHT / 2, image=tomato_img)
+timer_text = canvas.create_text(WIDTH / 2, 130, text="00:00", fill="white", font=FONT)
 canvas.grid(row=1, column=1)
 
 # timer text
-timer_text = Label(text="Timer", foreground=GREEN, background=YELLOW, font=(FONT_NAME, 50, FONT_CHARACTER))
-timer_text.grid(row=0, column=1)
+timer_label = Label(text="Timer", foreground=GREEN, background=YELLOW, font=(FONT_NAME, 50, FONT_CHARACTER))
+timer_label.grid(row=0, column=1)
 
 # create start button
-start_button = Button(text="Start")
+start_button = Button(text="Start", command=start_timer)  # starts the timer when clicked
 start_button.grid(row=2, column=0)
 
 # create restart button
@@ -62,6 +81,5 @@ restart_button.grid(row=2, column=2)
 # add check marks
 check_marks = Label(text="✔️", foreground=GREEN, background=YELLOW)
 check_marks.grid(row=3, column=1)
-
 
 window.mainloop()
