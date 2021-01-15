24061b5ed05f4188115f42ee30c929ab7d1e1cca
VictoriaGalvan <victoria.m.galvan@gmail.com>
2021-01-14 23:37:32 -0500

    Image loads to UI

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 # Created by Victoria M. Galvan
 
+# ---------------------------- IMPORTS ------------------------------- #
+from tkinter import *
+
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -9,6 +12,8 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+WIDTH = 200
+HEIGHT = 224
 
 # ---------------------------- TIMER RESET ------------------------------- # 
 
@@ -17,3 +22,15 @@ LONG_BREAK_MIN = 20
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Pomodoro Time Manager")
+
+# develop photo image
+tomato_img = PhotoImage(file="tomato.png")
+
+# create canvas
+canvas = Canvas(width=WIDTH, height=HEIGHT)
+canvas.create_image(WIDTH/2, HEIGHT/2, image=tomato_img)
+canvas.pack()
+
+window.mainloop()
