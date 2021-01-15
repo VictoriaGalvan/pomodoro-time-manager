503cb4b872056272574e24f90c0dbcae0d844e15
VictoriaGalvan <victoria.m.galvan@gmail.com>
2021-01-14 23:47:31 -0500

    Finished clock text and updated comments
    Have not yet created unit tests.
    
    Signed-off-by: VictoriaGalvan <victoria.m.galvan@gmail.com>

--- main.py
+++ main.py
@@ -4,14 +4,24 @@
 from tkinter import *
 
 # ---------------------------- CONSTANTS ------------------------------- #
+# Color Settings
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
+
+# Font Settings
 FONT_NAME = "Courier"
+FONT_SIZE = 35
+FONT_CHARACTER = "bold"
+FONT = (FONT_NAME, FONT_SIZE, FONT_CHARACTER)
+
+# Time Settings
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+
+# UI Settings
 WIDTH = 200
 HEIGHT = 224
 
@@ -22,15 +32,18 @@ HEIGHT = 224
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 # ---------------------------- UI SETUP ------------------------------- #
+# create window
 window = Tk()
 window.title("Pomodoro Time Manager")
+window.config(padx=100, pady=50)
 
 # develop photo image
 tomato_img = PhotoImage(file="tomato.png")
 
 # create canvas
 canvas = Canvas(width=WIDTH, height=HEIGHT)
-canvas.create_image(WIDTH/2, HEIGHT/2, image=tomato_img)
+canvas.create_image(WIDTH/2 + 3, HEIGHT/2, image=tomato_img)
+canvas.create_text(WIDTH/2 + 3, 130, text="00:00", fill="white", font=FONT)
 canvas.pack()
 
 window.mainloop()
