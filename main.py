43f6a738fe9d1f4801a294b4ede82c67ed2e2542
VictoriaGalvan <victoria.m.galvan@gmail.com>
2021-01-15 09:52:34 -0500

    Finished adding all UI components
    Signed-off-by: VictoriaGalvan <victoria.m.galvan@gmail.com>

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 
 # ---------------------------- IMPORTS ------------------------------- #
 from tkinter import *
+from tkinter.ttk import *
 
-# ---------------------------- CONSTANTS ------------------------------- #
+# ---------------------------- SETTINGS ------------------------------- #
 # Color Settings
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -44,6 +45,23 @@ tomato_img = PhotoImage(file="tomato.png")
 canvas = Canvas(width=WIDTH, height=HEIGHT, bg=YELLOW, highlightthickness=0)
 canvas.create_image(WIDTH/2, HEIGHT/2, image=tomato_img)
 canvas.create_text(WIDTH/2, 130, text="00:00", fill="white", font=FONT)
-canvas.pack()
+canvas.grid(row=1, column=1)
+
+# timer text
+timer_text = Label(text="Timer", foreground=GREEN, background=YELLOW, font=(FONT_NAME, 50, FONT_CHARACTER))
+timer_text.grid(row=0, column=1)
+
+# create start button
+start_button = Button(text="Start")
+start_button.grid(row=2, column=0)
+
+# create restart button
+restart_button = Button(text="Restart")
+restart_button.grid(row=2, column=2)
+
+# add check marks
+check_marks = Label(text="✔️", foreground=GREEN, background=YELLOW)
+check_marks.grid(row=3, column=1)
+
 
 window.mainloop()
